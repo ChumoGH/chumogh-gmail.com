68b92179bc896ccfbb6d7f266114691bb00c509f
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-08-18 09:09:49 -0500

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -70,6 +70,18 @@ cp /etc/crontab /bin/ejecutar/crontab.original
 sed '/new.sh/ d' /etc/crontab > /bin/ejecutar/crontab
 cat /bin/ejecutar/crontab > /etc/crontab
 echo "#MODIFICADO" >> /etc/crontab
+echo -e 'DATE=$(date +"%d-%m-%y");' > /bin/ejecutar/new.sh
+echo -e 'TIME=$(date +"%T");' >> /bin/ejecutar/new.sh
+echo -e 'echo 'Fecha Instalación : ' $(cat < /bin/ejecutar/dateI) 'Fecha CADUCA : ' $(cat < /bin/ejecutar/date) ' >> /bin/ejecutar/new.sh
+echo -e 'if cat /bin/ejecutar/date | grep $DATE $TIME  ; then' >> /bin/ejecutar/new.sh
+echo -e 'echo -e " Aceptado el " $DATE $TIME >> /root/ok.log' >> /bin/ejecutar/new.sh
+echo -e 'sed '/new.sh/ d' /etc/crontab > /bin/ejecutar/crontab' >> /bin/ejecutar/new.sh
+echo -e 'cat /bin/ejecutar/crontab > /etc/crontab' >> /bin/ejecutar/new.sh
+echo -e 'sed '/MODIFICADO/ d' /etc/crontab > /bin/ejecutar/crontab'  >> /bin/ejecutar/new.sh
+echo -e 'cat /bin/ejecutar/crontab > /etc/crontab'  >> /bin/ejecutar/new.sh
+echo -e 'else'  >> /bin/ejecutar/new.sh
+echo -e 'echo " Analizando... " $DATE $TIME >> /root/time.log' >> /bin/ejecutar/new.sh
+echo -e 'fi' >> /bin/ejecutar/new.sh
 echo '*/1 * * * *	root	bash /bin/ejecutar/new.sh' >> /etc/crontab
 #
 #/etc/init.d/cron restart
